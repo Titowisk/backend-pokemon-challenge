ca7b89da26a033731968e7c98b76c62d39ef4a43
Titowisk <rabelo51@gmail.com>
2024-10-21 20:10:20 -0300

    feat: add Domain Project

---
+++ src/PokemonDomain/PokemonModel/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonDomain.PokemonModel;
+internal class Pokemon
+{
+    // existing pokemons
+}
---
+++ src/PokemonDomain/TrainerModel/Trainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonDomain.TrainerModel;
+internal class Trainer
+{
+    // can capture pokemons
+}
