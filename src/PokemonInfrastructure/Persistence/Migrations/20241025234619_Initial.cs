39cd34a484a416a4fc83dcd02db120fcd6db0a3e
Titowisk <rabelo51@gmail.com>
2024-10-25 20:46:46 -0300

    feat: add initial migration

---
+++ src/PokemonInfrastructure/Persistence/Migrations/20241025234619_Initial.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonInfrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trainers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    CPF = table.Column<string>(type: "TEXT", maxLength: 11, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trainers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Weight = table.Column<float>(type: "REAL", nullable: false),
+                    Height = table.Column<float>(type: "REAL", nullable: false),
+                    Base64Sprite = table.Column<string>(type: "TEXT", nullable: false),
+                    TrainerId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pokemons_Trainers_TrainerId",
+                        column: x => x.TrainerId,
+                        principalTable: "Trainers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonEvolution",
+                columns: table => new
+                {
+                    EvolutionId = table.Column<int>(type: "INTEGER", nullable: false),
+                    InvolutionId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonEvolution", x => new { x.EvolutionId, x.InvolutionId });
+                    table.ForeignKey(
+                        name: "FK_PokemonEvolution_Pokemons_EvolutionId",
+                        column: x => x.EvolutionId,
+                        principalTable: "Pokemons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonEvolution_Pokemons_InvolutionId",
+                        column: x => x.InvolutionId,
+                        principalTable: "Pokemons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonType",
+                columns: table => new
+                {
+                    PokemonsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TypesId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonType", x => new { x.PokemonsId, x.TypesId });
+                    table.ForeignKey(
+                        name: "FK_PokemonType_Pokemons_PokemonsId",
+                        column: x => x.PokemonsId,
+                        principalTable: "Pokemons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonType_Types_TypesId",
+                        column: x => x.TypesId,
+                        principalTable: "Types",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonEvolution_InvolutionId",
+                table: "PokemonEvolution",
+                column: "InvolutionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pokemons_TrainerId",
+                table: "Pokemons",
+                column: "TrainerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonType_TypesId",
+                table: "PokemonType",
+                column: "TypesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PokemonEvolution");
+
+            migrationBuilder.DropTable(
+                name: "PokemonType");
+
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+
+            migrationBuilder.DropTable(
+                name: "Types");
+
+            migrationBuilder.DropTable(
+                name: "Trainers");
+        }
+    }
+}
